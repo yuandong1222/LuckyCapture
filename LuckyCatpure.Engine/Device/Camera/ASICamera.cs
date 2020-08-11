7ce8cbf1951835e3e3a08e2606e97acdae082afc
Dong Yuan <yuandong1222@gmail.com>
2020-08-11 17:55:18 +0800

    Update
    Signed-off-by: Dong Yuan <yuandong1222@gmail.com>

--- LuckyCatpure.Engine/Device/Camera/ASICamera.cs
+++ LuckyCatpure.Engine/Device/Camera/ASICamera.cs
@@ -1,12 +1,36 @@
-﻿using LuckyCatpure.Engine.Common;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
+
+using LuckyCatpure.Engine.Common;
+
+using ZWOptical.ASISDK;
 
 namespace LuckyCatpure.Engine.Device.Camera
 {
     public class ASICamera : ICamera
     {
+        public static ICamera[] ScanCameras()
+        {
+            List<ICamera> cameras = new List<ICamera>();
+
+            int camera_count = ASICameraDll2.ASIGetNumOfConnectedCameras();
+            if (camera_count == 0) return cameras.ToArray();
+
+            for (int i = 0; i < camera_count; i++)
+            {
+                var camera = new ASICamera();
+                camera.CameraID = i;
+
+                //TOTO: Build CameraInfo
+
+                cameras.Add(camera);
+            }
+            return cameras.ToArray();
+        }
+
+        private int CameraID;
+
         public CameraInfo Info { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
         public ErrorCode Connect()
@@ -28,5 +52,6 @@ namespace LuckyCatpure.Engine.Device.Camera
         {
             throw new NotImplementedException();
         }
+
     }
 }
--- LuckyCatpure.Engine/Device/Camera/CameraManager.cs
+++ LuckyCatpure.Engine/Device/Camera/CameraManager.cs
@@ -6,6 +6,9 @@ namespace LuckyCatpure.Engine.Device.Camera
 {
     public class CameraManager
     {
-
+        public ICamera[] ScanAllCamera()
+        {
+            return null;
+        }
     }
 }
