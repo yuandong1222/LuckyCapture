9c7e7cd8559bdf86264f911ae5e92201c613b7cc
Dong Yuan <yuandong1222@gmail.com>
2020-08-12 15:48:04 +0800

    Update
    Signed-off-by: Dong Yuan <yuandong1222@gmail.com>

--- LuckyCatpure.Engine/Device/Camera/CameraInfo.cs
+++ LuckyCatpure.Engine/Device/Camera/CameraInfo.cs
@@ -11,9 +11,28 @@ namespace LuckyCatpure.Engine.Device.Camera
 
         public CameraSDKType SDKType { get; set; }
 
-        BayerPattern BayerPattern { get; set; };
+        public bool IsColorCamera { get; set; }
+        BayerPattern BayerPattern { get; set; }
 
-        public bool CanCool { get; set; }        
+        public int MaxBin { get; set; }
+
+        public bool CanCool { get; set; }
+
+        public bool HasMechanicalShutter { get; set; }
+        public bool HasST4Port { get; set; }
+
+        public bool IsUSB3Host { get; set; }
+        public bool IsUSB3Camera { get; set; }
+
+        /// <summary>
+        /// Pixel Size in nm
+        /// </summary>
+        public int PixelSize { get; set; }
+        /// <summary>
+        /// 1500 mean 1.5e per ADU
+        /// </summary>
+        public int ElecPerADU { get; set; }
+
         public int MaxHeight { get; set; }
         public int MaxWeight { get; set; }
     }
